e37a2cc7c7296b98d7ecd61b7a3785524173bc83
lifayi <lifayi2008@163.com>
2021-01-05 21:32:29 +0800

    improve start script

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 function start_db () {
-    docker container stop hive-mongo || true && docker container rm -f hive-mongo || true
+    docker container list --all | grep hive-mongo > /dev/null \
+              && docker container stop hive-mongo > /dev/null \
+              && docker container rm -f hive-mongo > /dev/null
     docker run -d --name hive-mongo                     \
         --network hive                                  \
         -v ${PWD}/.mongodb-data:/data/db                \
@@ -33,19 +35,61 @@ function setup_venv () {
 }
 
 function start_docker () {
-    docker network create hive
-
-    start_db
-
     echo "Running using docker..."
-    docker container stop hive-node || true && docker container rm -f hive-node || true
-    docker build -t elastos/hive-node .
-    docker run -d --name hive-node               \
-      --network hive                          \
-      -v ${PWD}/.data:/src/data                \
-      -v ${PWD}/.env:/src/.env                \
-      -p 5000:5000                            \
+    docker version > /dev/null
+    if [ ! $? -eq 0 ];then
+        echo "You don't have docker installed. Please run the below commands to install docker"
+        echo "
+$ curl -fsSL https://get.docker.com -o get-docker.sh
+$ sudo sh get-docker.sh
+$ sudo usermod -aG docker $(whoami)"
+        exit
+    fi
+
+    cp .env.example .env
+
+    DID_MNEMONIC=$(grep 'DID_MNEMONIC' .env | sed 's/DID_MNEMONIC="//;s/"//')
+    echo -n "Your DID MNEMONIC: "
+    echo -e "\033[;36m ${DID_MNEMONIC} \033[0m"
+    echo -n "Confirm ? (y/n) "
+    read RESULT
+    RESULT=$(echo ${RESULT})
+    if [ ! "${RESULT}" == "y" ];then
+        echo -n "Please input your DID MNEMONIC: "
+        read DID_MNEMONIC
+        DID_MNEMONIC=$(echo ${DID_MNEMONIC})
+        [ "${DID_MNEMONIC}" = "" ] && echo "You don't input DID MNEMONIC" && exit 1
+        sed -i "/DID_MNEMONIC/s/^.*$/DID_MNEMONIC=\"${DID_MNEMONIC}\"/" .env
+    fi
+
+
+    echo -n "Please input your DID MNEMONIC PASSPHRASE: "
+    read DID_PASSPHRASE
+    DID_PASSPHRASE=$(echo ${DID_PASSPHRASE})
+    [ "${DID_PASSPHRASE}" != "" ] && sed -i "/DID_PASSPHRASE/s/^.*$/DID_PASSPHRASE=${DID_PASSPHRASE}/" .env
+    echo -n "Please input your DID MNEMONIC SECRET: "
+    read DID_STOREPASS
+    DID_STOREPASS=$(echo ${DID_STOREPASS})
+    [ "${DID_STOREPASS}" != "" ] && sed -i "/DID_STOREPASS/s/^.*$/DID_STOREPASS=${DID_STOREPASS}/" .env
+
+
+    docker network ls | grep hive > /dev/null || docker network create hive
+    start_db
+    docker container list --all | grep hive-node > /dev/null \
+              && docker container stop hive-node > /dev/null \
+              && docker container rm -f hive-node > /dev/null
+    docker build -t elastos/hive-node . > /dev/null
+    docker run -d --name hive-node  \
+      --network hive                \
+      -v ${PWD}/.data:/src/data     \
+      -v ${PWD}/.env:/src/.env      \
+      -p 5000:5000                  \
       elastos/hive-node
+    
+    echo -n "Checking... "
+    sleep 5
+    curl -s -X POST -H "Content-Type: application/json" -d '{"key":"value"}' http://localhost:5000/api/v1/echo > /dev/null
+    [ $? -eq 0 ] && echo -e "\033[;32m Success \033[0m" || echo -e "\033[;31m Failed \033[0m"
 }
 
 function start_direct () {
